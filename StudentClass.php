b2f851ac2b72059618df8385128d7efc900b01d4
Jacob <israelguirou7@gmail.com>
2022-01-08 15:38:56 -0500

    avance2

--- StudentClass.php
+++ StudentClass.php
@@ -13,12 +13,22 @@ class StudentClass{
     #se connecter
     /*public login($username, $password)
     {   
-       //
+        try{
+            $query = "Select * from Etudiant where username='$username' and password='$password'";
+            $e = $this->_db->query($query);
+            $result = $e->execute();
+
+        }
+        catch(PDOException $e){
+            echo $e;
+
+        }
+       
     }*/
-    #l'emploi du temps
+    #l'emploi du temps grace au nom ou id de l'etudiant
     public function getSchedule($nom)
     {
-        
+        //$schList[];
         $req=$this->_db->query("SELECT jours.Nom,professeur.idProf,heures.Debut,heures.Fin, groupe.numGroupe, cours.nomCours
         FROM seance,cours,groupe,jours,heures,professeur,personne,horaireseance
         WHERE personne.nom = '".$nom."' and 
@@ -29,35 +39,69 @@ class StudentClass{
         horaireseance.idSeance=seance.idSeance and
         horaireseance.IdJour=jours.IdJours and
         horaireseance.IdHeure=heures.IdHeures ");
-        $data= $req->fetch(PDO::FETCH_ASSOC);
+        $req->fetch(PDO::FETCH_ASSOC);
+        /*while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+            $schList[]=new PersonClass($data);
+        }
+        return $schList;
+        */
 
     }
     #ses camarades de classe: qui ont le meme idgroupe
     public function StudentList($numGroupe)
     {
+        //$stList[];
         $req=$this->_db->query("SELECT Personne.nom, Groupe.numGroupe
         FROM Personne, Etudiant, Groupe, GroupeEtud
         Where personne.role='etudiant' and numGroupe = '".$numGroupe."'  and
             personne.idPersonne = Etudiant.idPersonne and
             groupeEtud.idEtud = Etudiant.idEtudiant and
             groupeEtud.idGroupe = Groupe.idGroupe");
-        $data= $req->fetch(PDO::FETCH_ASSOC); 
+        $req->fetch(PDO::FETCH_ASSOC); 
+        /*while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+            $stList[]=new PersonClass($data);
+        }
+        return $stList;
+        */
     }
     #liste des groupes dont il fait partie
-    public function GroupList($numGroupe)
+    public function GroupList($idEtudiant)
     {
+        //$gpList = [];
         $req=$this->_db->query("SELECT Personne.nom, Groupe.numGroupe
         FROM Personne, Etudiant, Groupe, GroupeEtud
-        Where personne.role='etudiant' and numGroupe = '".$numGroupe."'  and
+        Where personne.role='etudiant' and Etudiant.idEtudiant='".$idEtudiant."' and
             personne.idPersonne = Etudiant.idPersonne and
             groupeEtud.idEtud = Etudiant.idEtudiant and
             groupeEtud.idGroupe = Groupe.idGroupe");
-        $data= $req->fetch(PDO::FETCH_ASSOC);   
-
+        $req->fetch(PDO::FETCH_ASSOC);   
+        /*while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+            $gpList[]=new PersonClass($data);
+        }
+        return $gpList;
+        */
     }
-    #les cours qu'il suit
-    public function CourseList()
-    {
+    #les cours qu'il suit par session
+    public function CourseList($idEtudiant)
+    {   
+        //$crList=[];
+        $req=$this->_db->query("SELECT Personne.nom, Cours.nomCours, Session.nom
+        FROM Personne, Etudiant, Cours, CoursEtudiant, Session
+        Where personne.role='etudiant' and Etudiant.idEtudiant ='".$idEtudiant."' and
+            Etudiant.idSession = Session.idSession and
+            Personne.idPersonne = Etudiant.idPersonne and
+            CoursEtudiant.idEtudiant = Etudiant.idEtudiant and
+            CoursEtudiant.idCours = Cours.idCours");
+        $req->fetch(PDO::FETCH_ASSOC);   
+        /*while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+            $crList[]=new PersonClass($data);
+        }
+        return $crList;
+        */
 
     }
 
